90b3271980ef32cce5ca548b1c13a9195f9fbea9
Pradeep <pradeepgorle4@gmail.com>
2024-08-22 10:59:51 +0530

    Variables Practice

---
+++ 02-Variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Pradeep: Hi Hemanth, how are you..?"
+echo "Hemanth: Hi Pradeep,Iam good, whatsup"
+echo "Pradeep: Nothing much Hemanth, busy as always. How is your life going"
+echo "Hemanth: Yeah, it's alright. Life's going great infact. Lets catch up sometime Pradeep"
