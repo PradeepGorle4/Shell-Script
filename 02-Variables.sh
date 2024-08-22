a4e607a4d9ead54ce43985f9e5a46717d81e681e
Pradeep <pradeepgorle4@gmail.com>
2024-08-22 17:54:57 +0530

    Variables practice

--- 02-Variables.sh
+++ 02-Variables.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "Pradeep: Hi Hemanth, how are you..?"
-echo "Hemanth: Hi Pradeep,Iam good, whatsup"
-echo "Pradeep: Nothing much Hemanth, busy as always. How is your life going"
-echo "Hemanth: Yeah, it's alright. Life's going great infact. Lets catch up sometime Pradeep"
+
+Person1=Pradeep
+Person2=Hemanth
+
+# Person1 and Person 2 are the variables here, assign any value to them as you wish and call them whenever neccessary with $ symbol
+
+echo "$Person1: Hi $Person2, how are you..?"
+echo "$Person2: Hi $Person1,Iam good, whatsup"
+echo "$Person1: Nothing much $Person2, busy as always. How is your life going"
+echo "$Person2: Yeah, it's alright. Life's going great infact. Lets catch up sometime $Person1"
