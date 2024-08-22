4b5b3b2b38a74e5afb27bdf5ca902fae5cbf42cc
Pradeep <pradeepgorle4@gmail.com>
2024-08-22 18:33:15 +0530

    Variables Practice - arguments

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -7,6 +7,9 @@ echo "Please enter your username: "
 
 read -s USERNAME  # This value you enter is automatically taken as username variable.
 
+# read -s , here -s is to hide the arguments you are passing while running this script. So here when you enter username it will not display in the terminal.
+
+
 echo "your username is : " $USERNAME 
 
 # Please do not print usernames and passwords in scripts, this is just for practice purpose.
