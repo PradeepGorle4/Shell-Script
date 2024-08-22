41c541e3cba1c993d16fd9a37dfc422be573ecac
Pradeep <pradeepgorle4@gmail.com>
2024-08-22 17:58:39 +0530

    Variables Practice

---
+++ 03-Variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is useful to know while troubleshooting as we can know when the program started executing
+
+DATE=$(date)
+
+echo "The script started executing at " $DATE
+
+# We can use $DATE while calling the variable or also can use ${DATE}
+
+#Done.
