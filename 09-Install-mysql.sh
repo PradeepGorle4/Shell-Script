996a2952cb39b013052a810032ec19fe97f53322
Pradeep <pradeepgorle4@gmail.com>
2024-08-24 09:40:38 +0530

    Installing softwares and Tools

---
+++ 09-Install-mysql.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script is for installing mysql and other softwares like git etc..
+
+ID=$(id -u) # id -u displays the user id. Only root user has user id as 0, rest user ID's are not zero in linux.
+
+if [ $ID -ne 0 ]  # if user id is not eqauls to 0, that is not root user
+then
+    echo "Please run this script with root user access"
+else
+    echo "you are root user"
+fi 
+
+
+
