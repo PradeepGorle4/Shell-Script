11f0a9a11be65cabed663c7d16a7a5232c9cd414
Pradeep <pradeepgorle4@gmail.com>
2024-08-26 16:44:51 +0530

    Installing softwares and Tools

---
+++ 10-Functions.sh
@@ -0,0 +1,30 @@
+#Script for Installing softwares
+echo "Checking for root access"
+
+ID=$(id -u)
+
+VALIDATE() {
+    if [$? -ne 0]
+then
+    echo "ERROR: Installing $1 ....FAILED"
+    exit 1
+else
+    echo "Installing $1 ....SUCCESS"
+fi
+}
+
+if [$ID -ne 0]
+then
+    echo "ERROR: Please run with root access"
+    exit 1
+else
+    echo "You are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE "MYSQL"
+
+yum install git -y
+
+VALIDATE "GIT"
